f4e3957b611d7bfdf074049a688c20592ad84a9f
hugosenari <hugosenari@gmail.com>
2022-09-03 19:16:51 -0300

    chore: re index paranim

---
+++ source/examples/examples.nimble
@@ -0,0 +1,19 @@
+# Package
+
+version       = "0.1.0"
+author        = "oakes"
+description   = "Paranim examples"
+license       = "Public Domain"
+srcDir        = "src"
+bin           = @["examples"]
+
+task dev, "Run dev version":
+  exec "nimble run examples"
+
+
+# Dependencies
+
+requires "nim >= 1.2.6"
+requires "paranim >= 0.12.0"
+requires "paratext >= 0.13.0"
+requires "stb_image >= 2.5"
---
+++ source/qn00dc1lvrfc76iwxfsb4fyl30ggzzv3-source/examples/src/ex01_image.nim
@@ -0,0 +1,34 @@
+import paranim/opengl
+import paranim/gl, paranim/gl/entities
+import stb_image/read as stbi
+import examples_common
+
+const image = staticRead("assets/aintgottaexplainshit.jpg")
+
+var entity: ImageEntity
+var width, height: int
+
+proc init*(game: var Game) =
+  doAssert glInit()
+
+  glEnable(GL_BLEND)
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+  glDisable(GL_CULL_FACE)
+  glDisable(GL_DEPTH_TEST)
+
+  var channels: int
+  var data = stbi.loadFromMemory(cast[seq[uint8]](image), width, height, channels, stbi.RGBA)
+  var uncompiledImage = initImageEntity(data, width, height)
+  entity = compile(game, uncompiledImage)
+
+proc tick*(game: Game) =
+  glClearColor(1f, 1f, 1f, 1f)
+  glClear(GL_COLOR_BUFFER_BIT)
+  glViewport(0, 0, GLsizei(game.frameWidth), GLsizei(game.frameHeight))
+
+  var e = entity
+  e.project(float(game.frameWidth), float(game.frameHeight))
+  e.translate(0f, 0f)
+  e.scale(float(width), float(height))
+  render(game, e)
+
